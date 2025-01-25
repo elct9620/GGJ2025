10ed51734b8d944d4c1e1ac014d519aef7bf4180
timcheng78 <tim10046009@gmail.com>
2025-01-25 18:51:05 +0800

    update Mary and Matt favorability

--- src/view/City.tsx
+++ src/view/City.tsx
@@ -22,7 +22,12 @@ export const City: FC<CitySnapshot> = ({ id, damage, npcStatus }) => {
 				<Building title="shell" bottom="33%" left="40%" damage={damage} />
 				<Building title="factory" bottom="46%" left="7%" damage={damage} />
 				<Building title="hospital" bottom="46%" left="54%" damage={damage} />
-				<Npc title="doctor" bottom="45%" left="42%" />
+				<Npc
+					title="doctor"
+					bottom="45%"
+					left="42%"
+					favorability={npcStatus.Mary.favorability}
+				/>
 				<Building title="office" bottom="35%" left="0%" damage={damage} />
 				<Npc
 					title="engineer"
@@ -31,7 +36,12 @@ export const City: FC<CitySnapshot> = ({ id, damage, npcStatus }) => {
 					favorability={npcStatus.Jack.favorability}
 				/>
 				<Building title="people" bottom="34%" left="60.5%" damage={damage} />
-				<Npc title="politician" bottom="33%" left="58%" />
+				<Npc
+					title="politician"
+					bottom="33%"
+					left="58%"
+					favorability={npcStatus.Matt.favorability}
+				/>
 			</div>
 		</Layout>
 	);
