f80ef8297993b0de5c53b92254c46e195e9dd164
Aotokitsuruya <contact@aotoki.me>
2025-01-25 16:20:45 +0800

    use life to calculate damage

--- src/entity/City.ts
+++ src/entity/City.ts
@@ -4,14 +4,13 @@ import { CityEvent, CityEventType, CityInitializedEvent } from "./CityEvent";
 type EventHandler<T extends CityEvent> = (event: T) => void;
 
 export class City {
-	static readonly MAX_LIFE_TIME = 60 * 10 * 1000; // 10 minutes
-	static readonly DAMAGE_SCALE = 10000;
+	static readonly MAX_LIFE = 60 * 10 * 1000; // 10 minutes
 	static readonly MAX_DAMAGE = 100;
 	static readonly MIN_DAMAGE = 0;
 	static readonly TARGET_DAMAGE_RATE = 0;
 
 	private _events: CityEvent[] = [];
-	private _damage = 0;
+	private _life = City.MAX_LIFE;
 	private _damageRate = 0;
 
 	constructor(public readonly id: string) {
@@ -30,14 +29,14 @@ export class City {
 	}
 
 	public get damage(): number {
-		return Math.max(
-			Math.min(this._damage / City.DAMAGE_SCALE, City.MAX_DAMAGE),
-			City.MIN_DAMAGE,
-		);
+		const currentDamage =
+			City.MAX_DAMAGE - Math.floor((this._life * 100) / City.MAX_LIFE);
+
+		return Math.max(Math.min(currentDamage, City.MAX_DAMAGE), City.MIN_DAMAGE);
 	}
 
 	public get damageRate(): number {
-		return Math.max(this._damageRate, 1, City.TARGET_DAMAGE_RATE);
+		return Math.max(this._damageRate, City.TARGET_DAMAGE_RATE);
 	}
 
 	public get isDestroyed(): boolean {
@@ -53,8 +52,7 @@ export class City {
 	}
 
 	private onCityInitializedEvent(event: CityInitializedEvent): void {
-		this._damage = 0;
-		this._damageRate =
-			(City.MAX_DAMAGE * City.DAMAGE_SCALE) / City.MAX_LIFE_TIME;
+		this._life = City.MAX_LIFE;
+		this._damageRate = 1;
 	}
 }
