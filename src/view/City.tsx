6a50c0b70d230048aa95222d5566e76c1ef0ac7f
timcheng78 <tim10046009@gmail.com>
2025-01-25 15:27:09 +0800

    change city to building

--- src/view/City.tsx
+++ src/view/City.tsx
@@ -10,29 +10,23 @@ import { css, cx } from "hono/css";
 const pRelative = css`
 	position: relative;
 `;
-`;
 
 const pAbsolute = css`
 	position: absolute;
-`;
 `;
 
 const container = css`
 	display: flex;
 	width: 768px;
 `;
-`;
 
 const shell = css`
 	bottom: 33%;
 	left: 40%;
 `;
-`;
 
 const factory = css`
 	bottom: 46%;
-	left: 7%;
-`;
 	left: 7%;
 `;
 
@@ -40,18 +34,15 @@ const hospital = css`
 	bottom: 46%;
 	right: 5%;
 `;
-`;
 
 const office = css`
 	bottom: 35%;
 	left: 0%;
 `;
-`;
 
 const people = css`
 	bottom: 34%;
 	right: 0%;
-`;
 `;
 
 export const City: FC<CitySnapshot> = ({ id }) => {
