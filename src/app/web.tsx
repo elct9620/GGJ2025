a929d3c79f422da7276aa30ee0507dad09d62282
Aotokitsuruya <contact@aotoki.me>
2025-01-25 11:15:52 +0800

    update web view to display city id

--- src/app/web.tsx
+++ src/app/web.tsx
@@ -4,7 +4,9 @@ import { getContainer } from "./container";
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/city/:id", async (c) => {
-	return c.html(<div>Hello World</div>);
+	const id = c.req.param("id");
+
+	return c.html(<div>CityId - {id}</div>);
 });
 
 export default app;
