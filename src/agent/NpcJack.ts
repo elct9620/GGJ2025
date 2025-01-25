399aac6c2a8699c7a14a3802f9914d4a263d7bb2
Aotokitsuruya <contact@aotoki.me>
2025-01-25 17:47:37 +0800

    add favorability change tool to Jack

--- src/agent/NpcJack.ts
+++ src/agent/NpcJack.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe-neo";
-import { type LanguageModelV1 } from "ai";
+import { z } from "zod";
+import { tool, type LanguageModelV1 } from "ai";
 import Mustache from "mustache";
 
 import { trackGenerateText } from "./utils";
@@ -7,6 +8,7 @@ import { OpenAi } from "@app/container";
 import { Config } from "@app/config";
 import system from "./prompts/jack.md";
 import { City } from "@entity/City";
+import { NpcName } from "@entity/Npc";
 
 @injectable()
 export class NpcJack {
@@ -23,6 +25,32 @@ export class NpcJack {
 			system: Mustache.render(system, {
 				config: this.config,
 			}),
+			maxSteps: 15,
+			tools: {
+				getFavorability: tool({
+					description: "Get the favorability of the NPC",
+					parameters: z.object({}),
+					execute: async () => {
+						const favorability = city.findNpc(NpcName.Jack)?.favorability;
+
+						return { favorability };
+					},
+				}),
+				changeFavorability: tool({
+					description:
+						"According to the interaction with the NPC change the favorability",
+					parameters: z.object({
+						change: z.number().int().min(-10).max(10),
+					}),
+					execute: async ({ change }) => {
+						const prevFavorability = city.findNpc(NpcName.Jack)?.favorability;
+						city.changeFavorability(NpcName.Jack, change);
+						const newFavorability = city.findNpc(NpcName.Jack)?.favorability;
+
+						return { change, prevFavorability, newFavorability };
+					},
+				}),
+			},
 		});
 
 		return reply.text;
