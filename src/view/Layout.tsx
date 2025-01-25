d4b85acdca36c6dee049fa5c522b072951ebd89b
Aotokitsuruya <contact@aotoki.me>
2025-01-25 13:52:47 +0800

    add style header

--- src/view/Layout.tsx
+++ src/view/Layout.tsx
@@ -1,10 +1,15 @@
 /** @jsxImportSource hono/jsx */
 
 import { FC } from "hono/jsx";
+import { Style } from "hono/css";
 
 export const Layout: FC = ({ children }) => {
 	return (
 		<html>
+			<head>
+				<title>Atlantis</title>
+				<Style />
+			</head>
 			<body>{children}</body>
 		</html>
 	);
