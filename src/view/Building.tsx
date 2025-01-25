544a3543084c072bd0a818794c65f633a5ec6a4d
timcheng78 <tim10046009@gmail.com>
2025-01-25 17:39:46 +0800

    update damage caculate

--- src/view/Building.tsx
+++ src/view/Building.tsx
@@ -6,7 +6,7 @@ const caculateDamaged = function (damage: number) {
 	if (damage < 25) return "_0.png";
 	if (damage < 50) return "_1.png";
 	if (damage < 75) return "_2.png";
-	if (damage < 100) return "_3.png";
+	return "_3.png";
 };
 
 export const Building: FC = ({ className, title, damage }) => {
